f26804b35cd43142af173c133a5b9692117d6654
Jesus Rodriguez <jesus.rodriguez.ascanio@gmail.com>
2023-11-30 16:25:56 -0500

    refactor buttons

--- lib/src/presentation/views/init_page.dart
+++ lib/src/presentation/views/init_page.dart
@@ -11,101 +11,85 @@ class _InitPageState extends State<InitPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
+      body: Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text("Iniciemos"),
-          SizedBox(height: 50),
-          FacebookButton(),
-          GoogleButton(),
-          AppleButton(),
-          GotoLogin()
+          const Text("Iniciemos"),
+          const SizedBox(height: 50),
+          SocialButton(
+            text: "Continua con Facebook",
+            icon: Image.asset("assets/social_icons/facebook.png", width: 20),
+            onClick: () => {},
+          ),
+          SocialButton(
+            text: "Continua con Google",
+            icon: Image.asset("assets/social_icons/google.png", width: 20) 
+          ),
+          SocialButton(
+            text: "Continua con Apple",
+            icon: Image.asset("assets/social_icons/apple.png", width: 20) 
+          ),
+          const GotoLogin()
         ],
       ),
     ));
   }
 }
 
-class FacebookButton extends StatelessWidget {
-  const FacebookButton({super.key});
+class SocialButton extends StatelessWidget {
+  final String text;
+  final Image icon;
+  final void Function()? onClick;
+  
+  const SocialButton({ 
+    super.key, 
+    this.text = "", 
+    this.onClick, 
+    required this.icon 
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
+    return GestureDetector(
+      onTap: onClick,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
           border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(12)),
-      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Image.asset("assets/social_icons/facebook.png", width: 20),
-        SizedBox(width: 5),
-        Text("Continua con Facebook",
-            style: TextStyle(fontWeight: FontWeight.bold)),
-      ]),
-    );
-  }
-}
-
-class GoogleButton extends StatelessWidget {
-  const GoogleButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(12)),
-      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Image.asset("assets/social_icons/google.png", width: 20),
-        SizedBox(width: 5),
-        Text("Continua con Google",
-            style: TextStyle(fontWeight: FontWeight.bold)),
-      ]),
-    );
-  }
-}
-
-class AppleButton extends StatelessWidget {
-  const AppleButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(12)),
-      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Image.asset("assets/social_icons/apple.png", width: 20),
-        SizedBox(width: 5),
-        Text("Continua con Apple",
-            style: TextStyle(fontWeight: FontWeight.bold)),
-      ]),
+          borderRadius: BorderRadius.circular(12)
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center, 
+          children: [
+            icon,
+            const SizedBox(width: 5),
+            Text(text, style: const TextStyle(fontWeight: FontWeight.bold)),
+          ]
+        ),
+      ),
     );
   }
 }
 
 class GotoLogin extends StatelessWidget {
   const GotoLogin({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
       child: ElevatedButton(
-          onPressed: () {},
-          child: Text("Iniciar con correo"),
-          style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.all(15),
-              shape: StadiumBorder(),
-              backgroundColor: Color.fromRGBO(77, 93, 250, 1))),
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.all(15),
+          shape: const StadiumBorder(),
+          backgroundColor: const Color.fromRGBO(77, 93, 250, 1)
+        ),
+        child: const Text("Iniciar con correo"),
+      ),
     );
   }
 }
